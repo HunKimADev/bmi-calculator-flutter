5c961c314ebe62884252ba568feae430c5d6cfe3
HunKimADev <hun.kim@outlook.com>
2020-08-17 15:52:49 +1200

    Changed theme colors and created the container layout

---
+++ lib/input_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: ReusableCard(Color(0xFF1D1E33)),
+                ),
+                Expanded(
+                  child: ReusableCard(Color(0xFF1D1E33)),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(Color(0xFF1D1E33)),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(Color(0xFF1D1E33)),
+                ),
+                Expanded(
+                  child: ReusableCard(Color(0xFF1D1E33)),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard(this.colour);
+
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
+  }
+}
